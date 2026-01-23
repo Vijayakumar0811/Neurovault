85185595dbe2d4eb24d55e397f5085106cccda54
Kalpanaababu <thangakalpana@gmail.com>
2026-01-23 09:33:25 +0530

    Update admin.py

--- security/admin.py
+++ security/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import LoginAttempt
 
-# Register your models here.
+@admin.register(LoginAttempt)
+class LoginAttemptAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "ip_address",
+        "status",
+        "success",
+        "created_at",
+        "blocked_until",
+    )
+    list_filter = ("status", "success")
+    search_fields = ("username", "ip_address")
