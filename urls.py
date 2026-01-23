683457a548059623e4e3101fe44f18ab42e46207
Kalpanaababu <thangakalpana@gmail.com>
2026-01-23 09:38:31 +0530

    Refactor URL patterns for login and admin views

--- urls.py
+++ urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import analyze_login
+from .views import (
+    login_view,
+    admin_dashboard_view,
+    admin_logs_api,
+    dashboard_stats_api,
+    clear_logs
+)
 
 urlpatterns = [
-    path("analyze/", analyze_login),
+    path("", login_view),
+    path("nv-vault/", admin_dashboard_view),
+    path("admin-logs/", admin_logs_api),
+    path("dashboard-stats/", dashboard_stats_api),
+    path("clear-logs/", clear_logs),
 ]
+
