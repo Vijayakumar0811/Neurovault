ba34b5c3ae0f63a84b74cceacc4fd6f627985b39
Kalpanaababu <thangakalpana@gmail.com>
2026-04-03 17:59:32 +0530

    Update urls.py

--- Neurovault-main/neurovault/urls.py
+++ Neurovault-main/neurovault/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Neurovault is LIVE 🚀")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("security.urls")),
+    path('', home),   # ✅ ADD THIS LINE
+    path('admin/', admin.site.urls),
+    path('api/', include('security.urls')),
 ]
